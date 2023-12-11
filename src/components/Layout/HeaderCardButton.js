6ceb3e747636d1f03a90fb259d76ca1290ebd978
Ayeshashaikh222 <ayeshashaikh0222@gmail.com>
2023-12-11 16:41:27 +0530

    added header

---
+++ src/components/Layout/HeaderCardButton.js
@@ -0,0 +1,14 @@
+import CardIcon from '../Card/CardIcon';
+import classes from './HeaderCardButton.module.css';
+
+const HeaderCardButton = (props) => {
+     return (
+        <button className={classes.button}>
+            <span className={classes.icon}><CardIcon /></span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>0</span>
+        </button>
+     )
+}
+
+export default HeaderCardButton;
