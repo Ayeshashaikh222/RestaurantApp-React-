b361ccb482527b98e65d07b4efa0c5226acde0c0
Ayeshashaikh222 <ayeshashaikh0222@gmail.com>
2023-12-13 12:09:32 +0530

    added card functionality

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,21 +1,23 @@
 import Input from '../UI/Input';
-import {useRef,} from 'react';
+import {useRef,useState} from 'react';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
     
     const amountInputRef = useRef();
    
     const submitHandler = (event) => {
-        event.PreventDefault();
+        event.preventDefault();
 
         const enteredAmount = amountInputRef.current.value;
         const enteredAmountNumber = +enteredAmount;
         
         if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            setAmountIsValid(false);
             return;
         }
-
+       props.onAddToCart(enteredAmountNumber);
     };
 
     return <form className={classes.form} onSubmit={submitHandler}>
@@ -28,6 +30,7 @@ const MealItemForm = (props) => {
             defaultValue: '1'
         }} />
         <button >+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount (1- 5)</p>}
     </form>
 }
 
